f9ebb8b1f72a4bc10ca7e807d7a197741dd78e32
DiogoAlves-a22407390 <a22407390@alunos.ulht.pt>
2026-05-18 12:25:37 +0000

    Opção (Ler mais) finalmente está a funcionar corretamente

---
+++ README.md
@@ -0,0 +1,8 @@
+Nome: Diogo Alves
+Numero: a22407390
+
+Login: admin
+Password: admin
+
+Login: Diogo_Alves
+Password: Gestor2026!
--- artigos/admin.py
+++ artigos/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Artigo, Comentario
  
-admin.site.register(Artigo)
-admin.site.register(Comentario)
- 
+@admin.register(Artigo)
+class ArtigoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'data_criacao')
+    search_fields = ('titulo', 'texto')
+    list_filter = ('data_criacao',)
+
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('autor', 'artigo', 'data_criacao')
